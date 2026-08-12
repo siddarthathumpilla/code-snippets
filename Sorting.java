7bec3edcf735ff523553b94936bf3dc6472e74de
siddarthathumpilla <tsiddartha_cse2405j8@mgit.ac.in>
2026-08-12 15:43:13 +0530

    Add Java sorting example

---
+++ Sorting.java
@@ -0,0 +1,17 @@
+// Java Sorting Example
+
+import java.util.Arrays;
+
+public class Sorting {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {5, 2, 8, 1, 3};
+
+        // Sort the array in ascending order
+        Arrays.sort(numbers);
+
+        System.out.println("Sorted array:");
+        System.out.println(Arrays.toString(numbers));
+    }
+}
